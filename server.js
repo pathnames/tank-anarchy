8b1029610b22718f12834b704464985dcc22e626
pjkpjk900 <pk58@buffalo.edu>
2020-06-26 19:45:13 -0400

    First part

--- server.js
+++ server.js
@@ -5,15 +5,50 @@ const socketIO = require('socket.io');
 const app = express();
 const server = http.Server(app);
 const io = socketIO(server);
-``
+
 app.set('port', 5000);
 app.use('/static', express.static(__dirname + '/static'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + 'index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 server.listen(5000, () => {
     console.log('Starting server on port 5000!');
 })
 
+//Websocket handlers
+io.on('connection', (socket) => {
+    setInterval(function(){
+        io.sockets.emit('message', 'hi');
+    }, 1000);
+});
+
+var players = {};
+io.on('connection', function(socket) {
+  socket.on('new player', function() {
+    players[socket.id] = {
+      x: 300,
+      y: 300
+    };
+  });
+  socket.on('movement', function(data) {
+    var player = players[socket.id] || {};
+    if (data.left) {
+      player.x -= 5;
+    }
+    if (data.up) {
+      player.y -= 5;
+    }
+    if (data.right) {
+      player.x += 5;
+    }
+    if (data.down) {
+      player.y += 5;
+    }
+  });
+});
+
+setInterval(function() {
+    io.sockets.emit('state', players);
+  }, 1000 / 60);
